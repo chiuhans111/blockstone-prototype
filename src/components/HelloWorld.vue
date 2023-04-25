9aa12b72764018d953e2c9af6a72760db048e2b8
Hans <chiuhans111@gmail.com>
2023-04-25 11:45:17 +0800

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,153 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div ref="canvasContainer"></div>
+    <p>超低面數的石頭</p>
   </div>
 </template>
 
 <script>
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { toRaw } from 'vue'
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+      scene: null,
+      renderer: null,
+      camera: null,
+      controls: null
+    }
+  },
+  mounted() {
+    // Create a scene, renderer, and camera
+    const scene = new THREE.Scene()
+    this.scene = scene
+    const renderer = new THREE.WebGLRenderer({ antialias: false, alpha: true })
+    renderer.setClearColor(0x000000, 0)
+    renderer.outputEncoding = THREE.sRGBEncoding
+    this.renderer = renderer
+    const canvas_width = 500
+    const canvas_height = 500
+
+    renderer.setSize(canvas_width, canvas_height)
+    this.$refs.canvasContainer.appendChild(renderer.domElement)
+
+    // Create a perspective camera
+    const camera = new THREE.PerspectiveCamera(
+      75, // field of view
+      canvas_width / canvas_height, // aspect ratio
+      0.1, // near clipping plane
+      1000 // far clipping plane
+    )
+    camera.position.z = 5
+
+    // Create a directional light with soft shadows
+    // const light = new THREE.DirectionalLight(0xffffff, 0)
+    // light.position.set(0, 0, 1)
+    // light.castShadow = true
+    // light.shadow.mapSize.width = 1024
+    // light.shadow.mapSize.height = 1024
+    // light.shadow.camera.near = 0.1
+    // light.shadow.camera.far = 100
+    // scene.add(light)
+
+
+    // Create an ambient light with a specific color and intensity
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1) // color: white, intensity: 0.5
+    scene.add(ambientLight)
+
+    // Create a GLTFLoader instance
+    const loader = new GLTFLoader()
+
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    this.controls = controls
+
+
+    // Load the glb model
+    loader.load(
+      require('@/assets/blockstudio_rock.glb').default, // or use the imported model variable if you used the import statement
+      (gltf) => {
+        // Add the loaded glTF scene to your Three.js scene
+        scene.add(gltf.scene)
+
+        gltf.scene.traverse((child) => {
+          if (child.isMesh) {
+            const material = new THREE.MeshStandardMaterial({
+              map: child.material.map,
+              normalMap: child.material.normalMap,
+              normalMapType: THREE.ObjectSpaceNormalMap,
+              roughness: 0.6, // Set roughness property to control the reflection roughness
+              metalness: 0,
+            })
+            child.material = material
+
+            // Set materials for child objects
+            console.log(child.material)
+
+            // child.material.map.wrapS = THREE.MirroredRepeatWrapping;
+            // child.material.map.wrapT = THREE.MirroredRepeatWrapping;
+            // child.material.map.magFilter = THREE.NearestFilter;
+            // child.material.map.minFilter = THREE.NearestFilter;
+          }
+        })
+
+
+
+        // Create OrbitControls for rotating the model with the mouse
+
+        controls.target = gltf.scene.position
+        controls.update()
+
+        console.log("load done")
+      },
+      (progress) => {
+        // Progress callback, e.g., to show loading progress
+        console.log('Loading progress:', progress)
+      },
+      (error) => {
+        // Error callback, e.g., to handle loading errors
+        console.error('Error loading glb model:', error)
+      }
+    )
+
+    // Set up shadow properties for the renderer
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+    // Render the scene with the loaded model
+    renderer.render(scene, camera)
+    this.camera = camera
+    this.scene = scene
+
+
+    this.animate()
+
+  }, methods: {
+    animate() {
+      requestAnimationFrame(this.animate)
+      // Update controls (if needed)
+      // Replace with your desired controls, e.g. TrackballControls, OrbitControls, etc.
+      this.controls.update()
+      this.renderer.render(toRaw(this.scene), toRaw(this.camera))
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style>
+canvas {
+  display: block;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+body {
+  background-color: #fff;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    // Add file-loader rule for glb and gltf files
+    config.module
+      .rule('glb')
+      .test(/\.(glb|gltf)$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        outputPath: 'assets/models/', // Specify the output directory for the models
+      });
+  },
 })
